c2d39b5e423b3c7226519fe5c60f49380bc3f350
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-20 11:37:14 +0800

    test: add unit tests, update imports, and variable name

--- tests/knightTravails.test.js
+++ tests/knightTravails.test.js
@@ -2,24 +2,30 @@ import { knightMoves } from '../src/knightMoves.js'
 
 describe('knightMoves tests', () => {
     test('1 move with single path', () => {
-        const moveSet = knightMoves([0,0],[1,2]);
+        const path = knightMoves([0,0],[1,2]);
         
-        expect(moveSet).toEqual([[0,0],[1,2]]);
+        expect(path).toEqual([[0,0],[1,2]]);
     })
 
     test('2 moves with multiple paths', () => {
-        const moveSet = knightMoves([0,0],[3,3]);
+        const path = knightMoves([0,0],[3,3]);
         
-        expect([[0,0],[2,1],[3,3]], [[0,0],[1,2],[3,3]]).toContainEqual(moveSet);
+        expect([[0,0],[2,1],[3,3]], [[0,0],[1,2],[3,3]]).toContainEqual(path);
 
-        const moveSet2 = knightMoves([3,3],[0,0]);
+        const path2 = knightMoves([3,3],[0,0]);
         
-        expect([[3,3],[2,1],[0,0]], [[3,3],[1,2],[0,0]]).toContainEqual(moveSet2);
+        expect([[3,3],[2,1],[0,0]], [[3,3],[1,2],[0,0]]).toContainEqual(path2);
     })
 
     test('6 moves with multiple paths', () => {
-        const moveSet = knightMoves([0,0],[7,7]);
+        const path = knightMoves([0,0],[7,7]);
         
-        expect([[0,0],[2,1],[4,2],[6,3],[4,4],[6,5],[7,7]], [[0,0],[2,1],[4,2],[6,3],[7,5],[5,6],[7,7]]).toContainEqual(moveSet);
+        expect([[0,0],[2,1],[4,2],[6,3],[4,4],[6,5],[7,7]], [[0,0],[2,1],[4,2],[6,3],[7,5],[5,6],[7,7]]).toContainEqual(path);
+    })
+
+    test('same start and end position', () => {
+        const path = knightMoves([7,6],[7,6]);
+        
+        expect(path).toEqual([[7,6]]);
     })
 })
