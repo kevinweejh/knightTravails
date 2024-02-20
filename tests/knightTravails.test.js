6237b3c8f18922c67b3b83dc8b80803034d4f1c7
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-20 14:46:15 +0800

    test: fix matcher for tests involving multiple paths

--- tests/knightTravails.test.js
+++ tests/knightTravails.test.js
@@ -9,18 +9,45 @@ describe('knightMoves tests', () => {
 
     test('2 moves with multiple paths', () => {
         const path = knightMoves([0,0],[3,3]);
+        const possiblePaths = [
+            [
+                [0,0],[2,1],[3,3]
+            ], 
+            [
+                [0,0],[1,2],[3,3]
+            ]
+        ]
         
-        expect([[[0,0],[2,1],[3,3]], [[0,0],[1,2],[3,3]]]).toContainEqual(path);
+        const pathMatches = possiblePaths.some((possiblePath) => JSON.stringify(possiblePath) === JSON.stringify(path));
+        expect(pathMatches).toBe(true);
 
         const path2 = knightMoves([3,3],[0,0]);
+        const possiblePaths2 = [
+            [
+                [3,3],[2,1],[0,0]
+            ], 
+            [
+                [3,3],[1,2],[0,0]
+            ]
+        ]
         
-        expect([[[3,3],[2,1],[0,0]], [[3,3],[1,2],[0,0]]]).toContainEqual(path2);
+        const pathMatches2 = possiblePaths2.some((possiblePath2) => JSON.stringify(possiblePath2) === JSON.stringify(path2));
+        expect(pathMatches2).toBe(true);
     })
 
     test('6 moves with multiple paths', () => {
         const path = knightMoves([0,0],[7,7]);
+        const possiblePaths = [
+            [
+                [0,0],[2,1],[4,2],[6,3],[4,4],[6,5],[7,7]
+            ], 
+            [
+                [0,0],[2,1],[4,2],[6,3],[7,5],[5,6],[7,7]
+            ]
+        ];
         
-        expect([[[0,0],[2,1],[4,2],[6,3],[4,4],[6,5],[7,7]], [[0,0],[2,1],[4,2],[6,3],[7,5],[5,6],[7,7]]]).toContainEqual(path);
+        const pathMatches = possiblePaths.some((possiblePath) => JSON.stringify(possiblePath) === JSON.stringify(path));
+        expect(pathMatches).toBe(true);
     })
 
     test('same start and end position', () => {
